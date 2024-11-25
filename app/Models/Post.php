d5ce910f3b7695771e8bd06ae75a13af878da1b0
yuta <yutaman236@gmail.com>
2024-11-25 08:22:52 +0000

    dev_basis02

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public function (int $limit_count = 10)
+
+    {
+        // updated_atで降順に並べたあと、limitで件数制限をかける
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
 }
+//public function getByLimit(int $limit_count = 10)
+// {
+//  return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
+// }
